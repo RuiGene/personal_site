02d085d127f0d2927791c43b68e083f4bf31638d
RuiGene <zrdanielyang@gmail.com>
2024-02-04 11:17:12 +1300

    added room for more projects

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -4,7 +4,7 @@ import stock_searcher from '../assets/stock_searcher.png';
 
 const Works = () => {
   return (
-    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+    <div name='work' className='w-full text-gray-300 bg-[#0a192f]'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8 w-full flex justify-center items-center flex-col'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-cyan-500'>
@@ -13,7 +13,7 @@ const Works = () => {
           <p className='py-6 text-2xl'>Check out some of my most recent projects!</p>
         </div>
 {/* Container */}
-        <div className='grid sm:grid-cols-2 md:grid-cols-2 gap-4'>
+        <div className='grid sm:grid-cols-3 md:grid-cols-2 gap-4'>
             {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(${stock_searcher})` }}
@@ -111,6 +111,55 @@ const Works = () => {
               </div>
             </div>
           </div>
+          {/* <div
+            style={{ backgroundImage: `url(${code})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+        // Hover effect
+            <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
+              <span className=' text-lg font-bold text-white tracking-wider'>
+                Coming soon...
+              </span>
+              <p className='text-center'>Placeholder text</p>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${code})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+          // Hoever effect
+            <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
+              <span className=' text-lg font-bold text-white tracking-wider'>
+                Coming soon...
+              </span>
+              <p className='text-center'>Placeholder text</p>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div> */}
+
         </div>
       </div>
     </div>
