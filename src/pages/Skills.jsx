f9f069bcfddd833b5c81cbd4b4c30ed2b2464b73
RuiGene <zrdanielyang@gmail.com>
2024-02-04 12:02:15 +1300

    updated logo

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { FaPython, FaSnowflake,FaHtml5, FaCss3Alt   } from "react-icons/fa";
+import {FaPython, FaSnowflake,FaHtml5, FaCss3Alt   } from "react-icons/fa";
 import { SiRstudio, SiPostgresql, SiPowerbi, SiTableau, SiDbt, SiJavascript, SiGrafana, SiAzuredevops    } from "react-icons/si";
-// import { Tooltip } from 'react-tooltip'
+// import { Tooltip } from 'react-tooltip';
 
 const Skills = () => {
   return (
@@ -10,11 +10,11 @@ const Skills = () => {
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div className=' w-full flex justify-center items-center flex-col mb-7'>
               <p className='text-4xl font-bold inline border-b-4 border-cyan-500 text-center '>Skills</p>
-              <p className='py-4 text-2xl'>I enjoy diving into and learning new things. Here's a list of technologies I've worked with</p>
+              <p className='py-4 text-2xl'>I enjoy diving into and learning new things. Here's a list of technologies I've worked with:</p>
           </div> 
           <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
               <div className=' hover:scale-110 duration-500 flex items-center justify-center'>
-                <FaPython size={100}/>
+              <FaPython size={100}/>
               </div>
               <div className=' hover:scale-110 duration-500 flex items-center justify-center'>
                 <SiRstudio size={100}/>
